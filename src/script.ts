491e9ecef6a0c6694afef652b0fe0a94ab41a15f
Pe-te-r <mburupeter8526@gmail.com>
2025-05-19 23:07:25 +0300

    read function

---
+++ src/script.ts
@@ -0,0 +1,36 @@
+// in this it is mostly preferred to be done for scripting tools or commandaline tools
+
+import { Client } from "pg";
+import dotenv from 'dotenv'
+
+dotenv.config()
+const get_connection=async() => {
+    const client =await new Client({
+        host: process.env.PGHOST,
+        user: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        database: process.env.PGDATABASE,
+        port: Number(process.env.PGPORT)
+    });
+    await client.connect()
+    return client
+}
+
+
+const readTable = async () => {
+    const client = await get_connection()
+    try {
+        const results = await client.query('SELECT * FROM users')
+        console.table(results.rows)
+    } catch (error) {
+        console.error('Error occured', error)
+    } finally {
+        client.end()
+    }
+}
+
+const
+
+export const main = async () => {
+    readTable()
+}
